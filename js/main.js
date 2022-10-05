f3c5094e8a3c98c0db5eb5660dd32eeb60c2f5e1
loordbarringtn <55858015+loordbarringtn@users.noreply.github.com>
2022-10-05 18:02:11 +0300

    second function added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-// eslint-disable-next-line no-unused-vars
-function randomInteger(min, max) {
-  if (min && max >= 0) {
+function getRandomInteger(min, max) {
+  if (min >= 0 && max >= 0) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+  return NaN;
 }
+//https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+
+
+function checkStringLength(string, length) {
+  if (string.length === length) {
+    return true;
+  }
+  return false;
+}
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/length
