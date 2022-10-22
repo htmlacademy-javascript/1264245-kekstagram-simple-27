fe513d92b9c98b6324863fec9d7c4458e69c9a48
loordbarringtn <55858015+loordbarringtn@users.noreply.github.com>
2022-10-22 23:43:31 +0300

    modules implemented

---
+++ js/data.js
@@ -0,0 +1,12 @@
+export const PHOTO_DESCRIPTION = [
+  'bag',
+  'empty',
+  'shopping bag',
+  'street',
+  'people',
+  'cat-ear headband ',
+  'Ultrasonic Motor',
+  'machine',
+];
+
+
---
+++ js/main.js
@@ -0,0 +1,3 @@
+import {makeGalleyPhotos} from './object-creation.js';
+
+console.log(makeGalleyPhotos());
