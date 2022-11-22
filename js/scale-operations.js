938dcc8fffadaab094e8e6a20186667912299a1a
loordbarringtn <55858015+loordbarringtn@users.noreply.github.com>
2022-11-23 01:57:26 +0300

    naming issues

--- js/scale-operations.js
+++ js/scale-operations.js
@@ -1,8 +1,8 @@
 const decreaseButton = document.querySelector('.scale__control--smaller');
 const increaseButton = document.querySelector('.scale__control--bigger');
-const scaleValue = document.querySelector('.scale__control--value');
-const image = document.querySelector('.img-upload__preview');
-const effectsList = document.querySelector('.effects__list');
+const scaleValueElement = document.querySelector('.scale__control--value');
+const imagePreviewElement = document.querySelector('.img-upload__preview');
+const effectsListElement = document.querySelector('.effects__list');
 
 const MIN_SCALE = 25;
 const MAX_SCALE = 100;
@@ -11,34 +11,34 @@ const DEFAULT_SCALE = 100;
 let currentScaleValue;
 
 const setDefaultSize = () => {
-  scaleValue.value = `${DEFAULT_SCALE}%`;
-  image.style.transform = `scale(${scaleValue.value / 100})`;
+  scaleValueElement.value = `${DEFAULT_SCALE}%`;
+  imagePreviewElement.style.transform = `scale(${scaleValueElement.value / 100})`;
 };
 
 increaseButton.addEventListener('click', () => {
-  let currentValue = parseInt(scaleValue.value, 10);
+  let currentValue = parseInt(scaleValueElement.value, 10);
   if (currentValue < MAX_SCALE) {
     currentValue += SCALE_STEP;
     currentScaleValue = currentValue;
   }
-  scaleValue.value = `${currentValue}%`;
-  image.style.transform = `scale(${currentValue / 100})`;
+  scaleValueElement.value = `${currentValue}%`;
+  imagePreviewElement.style.transform = `scale(${currentValue / 100})`;
 });
 
 decreaseButton.addEventListener('click', () => {
-  let currentValue = parseInt(scaleValue.value, 10);
+  let currentValue = parseInt(scaleValueElement.value, 10);
   if (currentValue > MIN_SCALE) {
     currentValue -= SCALE_STEP;
     currentScaleValue = currentValue;
   }
-  scaleValue.value = `${currentValue}%`;
-  image.style.transform = `scale(${currentValue / 100})`;
+  scaleValueElement.value = `${currentValue}%`;
+  imagePreviewElement.style.transform = `scale(${currentValue / 100})`;
 });
 
-effectsList.addEventListener('change', (evt) => {
+effectsListElement.addEventListener('change', (evt) => {
   const effectName = evt.target.value;
-  image.removeAttribute('class');
-  image.classList.add('img-upload__preview',`effects__preview--${effectName}`);
+  imagePreviewElement.removeAttribute('class');
+  imagePreviewElement.classList.add('img-upload__preview',`effects__preview--${effectName}`);
 });
 
 export {setDefaultSize, currentScaleValue};
